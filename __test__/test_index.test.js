08c165e5f26b7eabfd23ee17f5263e327eebd461
jonathon lee <jonnyleealas@gmail.com>
2020-10-01 15:01:31 -0700

    testing

--- __test__/test_index.test.js
+++ __test__/test_index.test.js
@@ -36,19 +36,6 @@ describe('Parse add', () => {
 
 });
 
-describe('Parse list', () => {
-  it('should parse --list', () => {
-    const input = new Input();
-    const command = input.getInput({ list: true });
-    expect(command.action).toBe('list');
-  });
-  it('should parse -l', () => {
-    const input = new Input();
-    const command = input.getInput({ l: true });
-    expect(command.action).toBe('list');
-  });
-});
-
 describe('parse category', () => {
 
   it('should parse -a with payload and --category', () => {
@@ -83,69 +70,37 @@ describe('parse category', () => {
 
 });
 
-xdescribe('Parse delete', () => {
+describe('Parse delete', () => {
   it('should parse --delete', () => {
     const input = new Input();
-    const command = input.parse({ delete: 'someid' });
+    const command = input.parse({ delete: 'icarly' });
     expect(command.action).toBe('delete');
-    expect(command.payload).toBe('someid');
+    expect(command.payload).toBe('icarly');
   });
   it('should parse -d', () => {
     const input = new Input();
-    const command = input.parse({ d: 'someid' });
+    const command = input.parse({ d: 'icarly' });
     expect(command.action).toBe('delete');
-    expect(command.payload).toBe('someid');
+    expect(command.payload).toBe('icarly');
   });
 
 });
 
-xdescribe('Validate', () => {
 
-  it('valid() respects a proper object', () => {
-    let options = new Input();
-    expect(options.valid()).toBe(true);
-  });
-
-  it('valid() respects an invalid object', () => {
-    let options = new Input();
-    options.command = { action: 'delete', payload: 'someid' }; // break it
-    expect(options.valid()).toBe(true);
-  });
-
-  it('valid() rejects an invalid object', () => {
-    let options = new Input();
-    options.command = {}; // break it
-    expect(options.valid()).toBe(false);
-  });
-
-  it('valid() rejects an invalid object', () => {
-    let options = new Input();
-    options.command = { action: 'add', payload: undefined }; // break it
-    expect(options.valid()).toBe(false);
-  });
-  it('valid() rejects an invalid object', () => {
-    let options = new Input();
-    options.command = { action: 'delete', payload: undefined }; // break it
-    expect(options.valid()).toBe(false);
-  });
-  
-
-});
-
-xdescribe('category', () => {
+describe('category', () => {
   it('should parse category with full switch', () => {
     let options = new Input();
-    const actual = options.parse({ add: 'buy milk', category: 'groceries' });
+    const actual = options.parse({ add: 'ninja turtles', category: 'groceries' });
     expect(actual.category).toBe('groceries');
   });
   it('should parse category with short switch', () => {
     let options = new Input();
-    const actual = options.parse({ add: 'buy milk', c: 'groceries' });
+    const actual = options.parse({ add: 'ninja turtles', c: 'groceries' });
     expect(actual.category).toBe('groceries');
   });
   it('should parse undefined category with missing switch', () => {
     let options = new Input();
-    const actual = options.parse({ add: 'buy milk' });
+    const actual = options.parse({ add: 'ninja turtles' });
     expect(actual.category).not.toBeDefined();
   });
 });
